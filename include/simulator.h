ef5e178f5f86442c1ffb6349278896911be62da3
FabianTsch <tschohl.fabian@gmail.com>
2024-01-24 11:57:29 +0100

    created virtual class structur for switch system

--- include/simulator.h
+++ include/simulator.h
@@ -8,10 +8,18 @@ typedef std::vector<double> state_t;
 typedef std::vector<double> input_t;
 typedef std::vector<double> trajectory_t;
 
-class Gantry{
+class System{
+    // This should be an abstract class but can't because of boost::odeint
+    // TODO: find a way to make this an abstract class
     public:
-        Gantry();
-        void operator()(const state_t &x, state_t &dxdt, const double t);
+        virtual void operator()(const state_t &x, state_t &dxdt, const double t){};
+        virtual ~System(){};
+};
+
+class GantryCompl : public System{
+    public:
+        GantryCompl();
+        void operator()(const state_t &x, state_t &dxdt, const double t) override;
 
         // Accerlation linear Equation
         friend int init_matrix_A(boost::numeric::ublas::matrix<double> &A, const state_t &x); 
@@ -29,10 +37,10 @@ class Gantry{
 
 class Simulator {
     public:
-        Simulator(Gantry &gantry);
+        Simulator(System &gantry);
         state_t integrate(state_t &x, const double dt,const double t0,const double tf);
     private:
-        Gantry &system;
+        System &system;
 };
 
 
--- src/simulator.cpp
+++ src/simulator.cpp
@@ -4,24 +4,24 @@
 #include "gantry.h"
 
 // Gantry class
-Gantry::Gantry(): A(11,11), b(11), pm(11) {
+GantryCompl::GantryCompl(): A(11,11), b(11), pm(11) {
     
 }
 
-state_t Gantry::ctr_function(const state_t &x, const double t) {
+state_t GantryCompl::ctr_function(const state_t &x, const double t) {
     input_t u(22);
     for(auto &i: u) i = 0.0;
     return u;
 }
 
-void Gantry::operator()(const state_t &x, state_t &dxdt, const double t) {
+void GantryCompl::operator()(const state_t &x, state_t &dxdt, const double t) {
     input_t u = ctr_function(x, t);
     std::copy(x.begin()+10, x.end(), dxdt.begin());
     this->b = this->get_accel(x,dxdt,u);
     std::copy(this->b.begin(), this->b.end(), dxdt.begin()+10);
 }
 
-boost::numeric::ublas::vector<double>& Gantry::get_accel(const state_t &x, const state_t &dxdt, const input_t &u) {
+boost::numeric::ublas::vector<double>& GantryCompl::get_accel(const state_t &x, const state_t &dxdt, const input_t &u) {
 
     init_matrix_A(this->A, x);
     init_matrix_B(this->b, x, dxdt, u);
@@ -35,7 +35,7 @@ boost::numeric::ublas::vector<double>& Gantry::get_accel(const state_t &x, const
 }
 
 // Simulator class
-Simulator::Simulator(Gantry &gantry): system(gantry) {
+Simulator::Simulator(System &system): system(system) {
 
 }
 
