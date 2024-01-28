78d94c88362a8277381bbe03bb8906a91d38df79
FabianTsch <tschohl.fabian@gmail.com>
2024-01-28 12:37:31 +0100

    add simple model

--- include/simulator.h
+++ include/simulator.h
@@ -18,8 +18,8 @@ class Gantry{
     private:
         //Boost Lib
         boost::numeric::ublas::vector<double> get_accel(const state_t &x, const state_t &dxdt, const input_t &u);
-        boost::numeric::ublas::matrix<double> get_accel_matrix_A(const state_t &x);
-        boost::numeric::ublas::vector<double> get_accel_vector_B(const state_t &x, const state_t &dxdt, const input_t &u);
+        boost::numeric::ublas::matrix<double> get_accel_matrix_A(const state_t &q);
+        boost::numeric::ublas::vector<double> get_accel_vector_B(const state_t &x, const input_t &u);
 
 };
 
@@ -33,41 +33,6 @@ class System{
     protected:
         // Acceleartion Equation
         input_t ctr_function(const state_t &x, const double t);
-};
-
-class GantryReduc : public System{
-    public:
-        size_t state_size;
-        GantryReduc();
-        void operator()(const state_t &x, state_t &dxdt, const double t) override;
-        state_t operator()(const state_t &x,const input_t &u) override;
-
-        // Accerlation linear Equation
-        friend void init_matrix_A_reduc(boost::numeric::ublas::matrix<double> &A, const state_t &x); 
-        friend void init_matrix_B_reduc(boost::numeric::ublas::vector<double> &b, const state_t &x, const state_t &xp, const input_t &u);
-
-    private:
-        boost::numeric::ublas::matrix<double> A;
-        boost::numeric::ublas::vector<double> b;
-        boost::numeric::ublas::permutation_matrix<std::size_t> pm;
-        boost::numeric::ublas::vector<double>& get_accel(const state_t &x, const state_t &dxdt, const input_t &u);
-};
-
-class GantryCompl : public System{
-    public:
-        size_t state_size;
-        GantryCompl();
-        void operator()(const state_t &x, state_t &dxdt, const double t) override;
-
-        // Accerlation linear Equation
-        friend int init_matrix_A_comp(boost::numeric::ublas::matrix<double> &A, const state_t &x); 
-        friend int init_matrix_B_comp(boost::numeric::ublas::vector<double> &b, const state_t &x, const state_t &xp, const input_t &u);
-    private:
-        boost::numeric::ublas::matrix<double> A;
-        boost::numeric::ublas::vector<double> b;
-        boost::numeric::ublas::vector<double>& get_accel(const state_t &x, const state_t &dxdt, const input_t &u);
-        boost::numeric::ublas::permutation_matrix<std::size_t> pm;
-        
 };
 
 class Simulator {
