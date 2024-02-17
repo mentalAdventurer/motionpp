9e7d38873a214be8472c95390cc9731eda8f7111
FabianTsch <tschohl.fabian@gmail.com>
2024-02-17 18:46:56 +0100

    change framework to make cost caluclation possible

--- src/cell_based_search.cpp
+++ src/cell_based_search.cpp
@@ -4,20 +4,10 @@
 
 #include "cspace.h"
 #include "graph.h"
-
-class Queue {
- public:
-  Queue(cspace::state_t x0);
-  void push(cspace::state_t x);
-  cspace::state_t pop();
-  bool empty();
-
- private:
-  std::vector<cspace::state_t> queue;
-};
+#include "queue.h"
 
 Graph cellBasedSearch(
-    cspace::state_t x0, cspace::state_t xg,
+    const cspace::state_t& x0, const cspace::state_t& xg,
     std::function<cspace::state_t(const cspace::state_t&, const cspace::input_t&)> dynamics,
     std::function<std::vector<cspace::input_trajectory_t>(const cspace::state_t&)> motionPrimitive) {
   Graph G(x0);
@@ -25,14 +15,14 @@ Graph cellBasedSearch(
   cspace::Voronoi P(10000, x0, xg);
   cspace::ReachedSet R(dynamics, motionPrimitive);
   while (!Q.empty() && !P.target_reached()) {
-    cspace::state_t x_cur = Q.pop();
+    const auto [x_cur, cost] = Q.pop();
     int time_steps = 3;
     R(x_cur, time_steps, 0.1);
     while (!R.empty()) {
       if (P.visit(R.front())) {
-        cspace::state_t x = R.pop_state();
-        cspace::input_trajectory_t u = R.pop_input();
-        Q.push(x);
+        const cspace::state_t x = R.pop_state();
+        const cspace::input_trajectory_t u = R.pop_input();
+        Q.push(x, u, cost);
         G.add_vertex(x);
         G.add_edge(x_cur, x, u);
       }
