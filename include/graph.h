44f75326127b4e4da214d760aa4816110300bc40
FabianTsch <tschohl.fabian@gmail.com>
2024-02-19 19:32:14 +0100

    changed graph to pointer structur

--- include/graph.h
+++ include/graph.h
@@ -28,29 +28,29 @@ class Graph{
     private:
         std::list<Vertex> vertices;
         std::list<Edge> edges;
-        std::unordered_map<cspace::state_t, Vertex*> vertex_map;
+        std::unordered_map<std::shared_ptr<const cspace::state_t>, Vertex*> vertex_map;
 
 
     public:
-        Graph(cspace::state_t x0);
-        void add_vertex(const cspace::state_t& x);
-        void add_edge(const cspace::state_t& x1, const cspace::state_t& x2, const cspace::input_trajectory_t& u);
-        std::list<cspace::input_t> get_input(const cspace::state_t& x);
+        Graph(std::shared_ptr<const cspace::state_t> x0_ptr);
+        void add_vertex(std::shared_ptr<const cspace::state_t> x);
+        void add_edge(std::shared_ptr<const cspace::state_t> x1, std::shared_ptr<const cspace::state_t> x2, std::shared_ptr<const cspace::input_trajectory_t> u);
+        std::list<cspace::input_t> get_input(std::shared_ptr<const cspace::state_t> x);
         
 };
 
 struct Graph::Vertex {
-    cspace::state_t state;
+    std::shared_ptr<const cspace::state_t> state;
     std::list<Edge*> edges_out;
     Edge* edge_in = nullptr;
-    Vertex(const cspace::state_t& x) : state(x) {}
+    Vertex(std::shared_ptr<const cspace::state_t> x) : state(x) {}
 };
 
 struct Graph::Edge {
     Vertex* source;
     Vertex* target;
-    cspace::input_trajectory_t input;
-    Edge(Vertex* s, Vertex* t, const cspace::input_trajectory_t& u) : source(s), target(t), input(u) {}
+    std::shared_ptr<const cspace::input_trajectory_t> input;
+    Edge(Vertex* s, Vertex* t, std::shared_ptr<const cspace::input_trajectory_t> u) : source(s), target(t), input(u) {}
 };
 
 #endif // GRAPH_H
--- src/cell_based_search.cpp
+++ src/cell_based_search.cpp
@@ -10,8 +10,10 @@ Graph cellBasedSearch(
     const cspace::state_t& x0, const cspace::state_t& xg,
     std::function<cspace::state_t(const cspace::state_t&, const cspace::input_t&)> dynamics,
     std::function<std::vector<cspace::input_trajectory_t>(const cspace::state_t&)> motionPrimitive) {
-  Graph G(x0);
-  Queue Q(x0);
+
+  std::shared_ptr<const cspace::state_t> x0_ptr = std::make_shared<const cspace::state_t>(x0);
+  Graph G(x0_ptr);
+  Queue Q(x0_ptr);
   cspace::Voronoi P(10000, x0, xg);
   cspace::ReachedSet R(dynamics, motionPrimitive);
   while (!Q.empty() && !P.target_reached()) {
@@ -20,8 +22,8 @@ Graph cellBasedSearch(
     R(x_cur, time_steps, 0.1);
     while (!R.empty()) {
       if (P.visit(R.front())) {
-        const cspace::state_t x = R.pop_state();
-        const cspace::input_trajectory_t u = R.pop_input();
+        auto x = R.pop_state_ptr();
+        auto u = R.pop_input_ptr();
         Q.push(x, u, cost);
         G.add_vertex(x);
         G.add_edge(x_cur, x, u);
