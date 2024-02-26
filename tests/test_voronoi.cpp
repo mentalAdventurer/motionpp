afd310f6634dfd3324c908ac9c3e970a6c264cc8
FabianTsch <tschohl.fabian@gmail.com>
2024-02-26 20:54:44 +0100

    adapt test to new constructor

--- tests/test_voronoi.cpp
+++ tests/test_voronoi.cpp
@@ -11,13 +11,15 @@ namespace cs = cspace;
 TEST(Voronoi, constructor){
   state_t x0 = {0, 0, 0, 0};
   state_t xg = {1, 0, 0, 0};
-  Voronoi P(10000, x0, xg);
+  Options::StateLimits limits = {{-1, 1}, {-1, 1}, {-1, 1}, {-1, 1}};
+  Voronoi P(10000, x0, xg, limits);
 }
 
 TEST(Voronoi, visit){
   state_t x0 = {0, 0, 0, 0};
   state_t xg = {1, 0, 0, 0};
-  Voronoi P(10000, x0, xg);
+  Options::StateLimits limits = {{-1, 1}, {-1, 1}, {-1, 1}, {-1, 1}};
+  Voronoi P(10000, x0, xg, limits);
   state_t x = {0.5, 0, 0, 0};
   EXPECT_TRUE(P.visit(x));
   EXPECT_FALSE(P.visit(x));
@@ -26,7 +28,8 @@ TEST(Voronoi, visit){
 TEST(Voronoi,target_reached){
   state_t x0 = {0, 0, 0, 0};
   state_t xg = {1, 0, 0, 0};
-  Voronoi P(10000, x0, xg);
+  Options::StateLimits limits = {{-1, 1}, {-1, 1}, {-1, 1}, {-1, 1}};
+  Voronoi P(10000, x0, xg, limits);
   EXPECT_FALSE(P.target_reached()) << "Voronoi just initialised. Target should not be reached yet";
   state_t x = {10, 2, 7, 4};
   P.visit(x);
