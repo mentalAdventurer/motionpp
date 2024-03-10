89d1789e36bcc8d590a0b9306b0593210a3dd570
FabianTsch <tschohl.fabian@gmail.com>
2024-03-10 15:09:37 +0100

    used auto formating on cspace.h

--- include/cspace.h
+++ include/cspace.h
@@ -16,48 +16,51 @@ typedef std::vector<std::vector<double>> input_trajectory_t;
 typedef std::shared_ptr<const state_t> state_ptr;
 using input_traj_ptr = std::shared_ptr<const input_trajectory_t>;
 using InputTrajPtrTimePair = std::pair<input_traj_ptr, float>;
-using trajTuple = std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>; 
-using fun_simulator = std::function<std::vector<double>(std::vector<double>::const_iterator,
-                                                        std::vector<double>::const_iterator, std::size_t, std::size_t, float)>;
+using trajTuple = std::tuple<std::vector<double>, std::vector<double>, std::vector<double>>;
+using fun_simulator =
+    std::function<std::vector<double>(std::vector<double>::const_iterator,
+                                      std::vector<double>::const_iterator, std::size_t, std::size_t, float)>;
 using fun_inputs = std::function<std::pair<std::vector<double>, std::vector<float>>(const cspace::state_t&)>;
-using fun_motion_primitive = std::function<std::vector<trajTuple>(const state_t&)>; 
+using fun_motion_primitive = std::function<std::vector<trajTuple>(const state_t&)>;
 
-struct DynamicObstacle{
-    gkPolytope polytope;
-    std::vector<double> polytope_data;
-    std::unique_ptr<double*[]> compatability_array;
-    int num_vertices;
-    state_t x_cur;
-    std::function<std::vector<double>&(std::vector<double>&, const state_t&, const state_t&)> transform_polytope;
-    DynamicObstacle(const std::vector<double>& vertices, int num_vertices, decltype(transform_polytope) transform_fun,state_t x_cur);
-    ~DynamicObstacle(){};
-    DynamicObstacle(const DynamicObstacle& other);
-    DynamicObstacle& operator=(const DynamicObstacle& other);
-    DynamicObstacle(DynamicObstacle&& other) noexcept;
-    DynamicObstacle& operator=(DynamicObstacle&& other) noexcept;
-    std::vector<double>& transform_obstacle(std::vector<double>&, const state_t& x0, const state_t& xg); 
+struct DynamicObstacle {
+  gkPolytope polytope;
+  std::vector<double> polytope_data;
+  std::unique_ptr<double*[]> compatability_array;
+  int num_vertices;
+  state_t x_cur;
+  std::function<std::vector<double>&(std::vector<double>&, const state_t&, const state_t&)>
+      transform_polytope;
+  DynamicObstacle(const std::vector<double>& vertices, int num_vertices,
+                  decltype(transform_polytope) transform_fun, state_t x_cur);
+  ~DynamicObstacle(){};
+  DynamicObstacle(const DynamicObstacle& other);
+  DynamicObstacle& operator=(const DynamicObstacle& other);
+  DynamicObstacle(DynamicObstacle&& other) noexcept;
+  DynamicObstacle& operator=(DynamicObstacle&& other) noexcept;
+  std::vector<double>& transform_obstacle(std::vector<double>&, const state_t& x0, const state_t& xg);
 };
 
-struct StaticObstacle{
-    gkPolytope polytope;
-    std::vector<double> polytope_data;
-    std::unique_ptr<double*[]> compatability_array;
-    int num_vertices;
-    ~StaticObstacle(){};
-    StaticObstacle(const std::vector<double>& vertices, int num_vertices);
-    StaticObstacle(const StaticObstacle& other);
-    StaticObstacle& operator=(const StaticObstacle& other);
-    StaticObstacle(StaticObstacle&& other) noexcept;
-    StaticObstacle& operator=(StaticObstacle&& other) noexcept;
+struct StaticObstacle {
+  gkPolytope polytope;
+  std::vector<double> polytope_data;
+  std::unique_ptr<double*[]> compatability_array;
+  int num_vertices;
+  ~StaticObstacle(){};
+  StaticObstacle(const std::vector<double>& vertices, int num_vertices);
+  StaticObstacle(const StaticObstacle& other);
+  StaticObstacle& operator=(const StaticObstacle& other);
+  StaticObstacle(StaticObstacle&& other) noexcept;
+  StaticObstacle& operator=(StaticObstacle&& other) noexcept;
 };
 
 struct Options {
-    using StateLimits = std::vector<std::pair<double,double>>;
-    std::size_t NumberOfPoints;
-    StateLimits limits;
-    std::vector<DynamicObstacle> dynamic_obstacles;
-    std::vector<StaticObstacle> static_obstacles;
-    Options(std::size_t NumberOfPoints,StateLimits limits) : NumberOfPoints(NumberOfPoints), limits(limits) {}
+  using StateLimits = std::vector<std::pair<double, double>>;
+  std::size_t NumberOfPoints;
+  StateLimits limits;
+  std::vector<DynamicObstacle> dynamic_obstacles;
+  std::vector<StaticObstacle> static_obstacles;
+  Options(std::size_t NumberOfPoints, StateLimits limits) : NumberOfPoints(NumberOfPoints), limits(limits) {}
 };
 
 class Voronoi {
@@ -100,17 +103,19 @@ class ReachedSet {
   fun_inputs generateInput = nullptr;
   fun_motion_primitive primitives = nullptr;
   Options opt;
-  bool collision(std::vector<double>::iterator,std::vector<double>::iterator,std::size_t);
+  bool collision(std::vector<double>::iterator, std::vector<double>::iterator, std::size_t);
   void add_reached_state(std::vector<double>::const_iterator first, std::size_t states_dim);
   void add_reached_state(std::vector<double>::const_iterator first, std::vector<double>::const_iterator last);
   void add_reached_input(std::vector<double>::const_iterator first, float time, std::size_t traj_dim,
                          std::size_t states_dim);
-  void add_reached_input(std::vector<double>::const_iterator first,std::vector<double>::const_iterator last, float time,std::size_t input_dim);
+  void add_reached_input(std::vector<double>::const_iterator first, std::vector<double>::const_iterator last,
+                         float time, std::size_t input_dim);
   std::vector<std::vector<double>> convertTo2D(std::vector<double>::const_iterator first,
                                                std::vector<double>::const_iterator last,
                                                std::size_t state_dim);
-  std::vector<double> eulerIntegrate(std::vector<double>::const_iterator x0, std::vector<double>::const_iterator u_traj,
-                                     std::size_t state_dim, std::size_t traj_dim, float dt);
+  std::vector<double> eulerIntegrate(std::vector<double>::const_iterator x0,
+                                     std::vector<double>::const_iterator u_traj, std::size_t state_dim,
+                                     std::size_t traj_dim, float dt);
   std::vector<state_ptr> states;
   std::vector<float> times;
   std::vector<input_traj_ptr> input_traj;
