8335d308cf53ae65b9c14cffc76b99516d7d0290
FabianTsch <tschohl.fabian@gmail.com>
2024-03-19 17:01:36 +0100

    simplified motion primitive

--- examples/2DMotionPrimitives/main.cpp
+++ examples/2DMotionPrimitives/main.cpp
@@ -13,82 +13,12 @@ struct MotionPrimitive {
   std::vector<trajTuple> template_primitives;
   std::vector<trajTuple> template_primitives_neutral;
   // std::vector<double> speeds = {0.1, 0.2, 0.3, 0.4, 0.5};
-  std::vector<double> speeds = {0.1, 0.3, 0.5};
-  double max_speed = 0.5;
+  std::vector<double> speeds = {0.0,0.1,-0.1,0.3,-0.3,0.5,-0.5};
+  double max_speed = 0.55;
   MotionPrimitive();
   std::vector<trajTuple> operator()(const cspace::state_t x);
 };
 
-MotionPrimitive::MotionPrimitive() {
-  static constexpr double start_position = 0.0, start_speed = 0.0;
-  template_primitives.reserve(speeds.size());
-
-  for (std::size_t i = 0; i < speeds.size(); i++) {
-    template_primitives.push_back(get_zv_trajectory(start_position, start_speed, speeds[i]));
-  }
-  // Create Neutral Motion Primitive
-  double time_step = param::dt;
-  double time_end = 0.2;
-
-  std::vector<double> time;
-  time.reserve(time_end / time_step);
-  for (double t = 0; t <= time_end; t += time_step) time.push_back(t);
-  std::vector<double> traj_neutral(time.size() * 4, 0.0);
-  std::vector<double> input_neutral(time.size(), 0.0);
-
-  template_primitives_neutral.push_back(std::make_tuple(time, traj_neutral, input_neutral));
-}
-
-std::vector<trajTuple> MotionPrimitive::operator()(const cspace::state_t x) {
-  std::size_t dim = x.size();
-  std::vector<trajTuple> primitives;
-  primitives.reserve(template_primitives.size());
-  std::vector<double> traj;
-  // Neutral Motion Primitive
-  for (std::size_t i = 0; i < template_primitives_neutral.size(); i++) {
-    for (std::size_t j = 0; j < std::get<1>(template_primitives_neutral[i]).size(); j += dim) {
-      double t = std::get<0>(template_primitives_neutral[i])[j / 4];
-      traj.push_back(std::get<1>(template_primitives_neutral[i])[j]);
-      traj.push_back(std::get<1>(template_primitives_neutral[i])[j + 1]);
-      traj.push_back(std::get<1>(template_primitives_neutral[i])[j + 2] + x[2] + x[3] * t);
-      traj.push_back(std::get<1>(template_primitives_neutral[i])[j + 3] + x[3]);
-    }
-    primitives.push_back(std::make_tuple(std::get<0>(template_primitives_neutral[i]), traj,
-                                         std::get<2>(template_primitives_neutral[i])));
-  }
-
-  // Positve Motionprimitve
-  for (std::size_t i = 0; i < speeds.size(); i++) {
-    traj.clear();
-    // No step bigger than max speed nor exceeding the max speed
-    if (std::abs(speeds[i] + x[3]) <= max_speed) {
-      for (std::size_t j = 0; j < std::get<1>(template_primitives[i]).size(); j += dim) {
-        double t = std::get<0>(template_primitives[i])[j / 4];
-        traj.push_back(std::get<1>(template_primitives[i])[j]);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 1]);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 2] + x[2] + x[3] * t);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 3] + x[3]);
-      }
-      primitives.push_back(
-          std::make_tuple(std::get<0>(template_primitives[i]), traj, std::get<2>(template_primitives[i])));
-      traj.clear();
-    }
-    if (std::abs(-speeds[i] + x[3]) <= max_speed) {
-      // Negative Motion Primitive
-      for (std::size_t j = 0; j < std::get<1>(template_primitives[i]).size(); j += dim) {
-        double t = std::get<0>(template_primitives[i])[j / 4];
-        traj.push_back(-std::get<1>(template_primitives[i])[j]);
-        traj.push_back(-std::get<1>(template_primitives[i])[j + 1]);
-        traj.push_back(-std::get<1>(template_primitives[i])[j + 2] + x[2] + x[3] * t);
-        traj.push_back(-std::get<1>(template_primitives[i])[j + 3] + x[3]);
-      }
-      std::vector<double> neg_input = std::get<2>(template_primitives[i]);
-      std::transform(neg_input.begin(), neg_input.end(), neg_input.begin(), [](double u) { return -u; });
-      primitives.push_back(std::make_tuple(std::get<0>(template_primitives[i]), traj, neg_input));
-    }
-  }
-  return primitives;
-}
 
 std::vector<double>& move_obstacle(std::vector<double>& obstacle, const cspace::state_t& x0,
                                   const cspace::state_t& xg) {
@@ -100,6 +30,51 @@ std::vector<double>& move_obstacle(std::vector<double>& obstacle, const cspace::
   }
 
   return obstacle;
+}
+
+MotionPrimitive::MotionPrimitive() {
+  // Create Neutral Motion Primitive
+  double time_step = param::dt;
+  double time_end = 0.2;
+
+  std::vector<double> time;
+  time.reserve(time_end / time_step);
+  for (double t = 0; t <= time_end; t += time_step) time.push_back(t);
+  std::vector<double> traj_neutral(time.size() * 4, 0.0);
+  std::vector<double> input_neutral(time.size(), 0.0);
+
+  template_primitives.push_back(std::make_tuple(time, traj_neutral, input_neutral));
+
+  // Create Motion Primitives
+  static constexpr double start_position = 0.0, start_speed = 0.0;
+  template_primitives.reserve(speeds.size());
+
+  for (std::size_t i = 1; i < speeds.size(); i++) {
+    template_primitives.push_back(get_zv_trajectory(start_position, start_speed, speeds[i]));
+  }
+}
+
+std::vector<trajTuple> MotionPrimitive::operator()(const cspace::state_t x) {
+  std::size_t dim = x.size();
+  std::vector<trajTuple> primitives;
+  primitives.reserve(template_primitives.size());
+
+  for (std::size_t i = 0; i < template_primitives.size(); i++) {
+    std::vector<double> traj;
+    // No step bigger than max speed nor exceeding the max speed
+    if (std::abs(speeds[i] + x[3]) <= max_speed) {
+      for (std::size_t j = 0; j < std::get<1>(template_primitives[i]).size(); j += dim) {
+        double t = std::get<0>(template_primitives[i])[j / 4];
+        traj.push_back(std::get<1>(template_primitives[i])[j]);
+        traj.push_back(std::get<1>(template_primitives[i])[j + 1]);
+        traj.push_back(std::get<1>(template_primitives[i])[j + 2] + x[2] + x[3] * t);
+        traj.push_back(std::get<1>(template_primitives[i])[j + 3] + x[3]);
+      }
+      primitives.push_back(
+          std::make_tuple(std::get<0>(template_primitives[i]), traj, std::get<2>(template_primitives[i])));
+    }
+  }
+  return primitives;
 }
 
 int main() {
@@ -121,7 +96,7 @@ int main() {
   //opt.dynamic_obstacles.push_back(upper_cart);
   //opt.static_obstacles.push_back(block);
 
-  cspace::state_t x_test = {0, 0, 0, 0};
+  cspace::state_t x_test = {0, 0, 0.0, 0.1};
   auto primitives_x0 = primitives(x_test);
   std::cout << "Number of primitives: " << primitives_x0.size() << std::endl;
   plot_states_and_input(std::get<0>(primitives_x0[1]), std::get<1>(primitives_x0[1]),
