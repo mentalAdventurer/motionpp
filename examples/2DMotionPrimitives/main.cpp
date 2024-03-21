6015cef14845136217cb938fe76e34cec61ac7d4
FabianTsch <tschohl.fabian@gmail.com>
2024-03-21 08:56:13 +0100

    create functional draft of 2DExample

--- examples/2DMotionPrimitives/main.cpp
+++ examples/2DMotionPrimitives/main.cpp
@@ -11,17 +11,17 @@ using primitiveTuple = std::tuple<std::vector<double>, std::vector<double>, std:
 
 struct MotionPrimitive {
   std::vector<trajTuple> template_primitives;
-  std::vector<trajTuple> template_primitives_neutral;
-  // std::vector<double> speeds = {0.1, 0.2, 0.3, 0.4, 0.5};
-  std::vector<double> speeds = {0.0,0.1,-0.1,0.3,-0.3,0.5,-0.5};
+  std::vector<double> speeds = {0.0 ,0.5,-0.5};
+  std::vector<std::pair<double, double>> speeds_xy;
   double max_speed = 0.55;
   MotionPrimitive();
   std::vector<trajTuple> operator()(const cspace::state_t x);
+  std::vector<trajTuple> generate_1d_primitives();
+  void equalize_primitive_length(std::vector<trajTuple>& primitive1, std::vector<trajTuple>& primitive2);
 };
 
-
 std::vector<double>& move_obstacle(std::vector<double>& obstacle, const cspace::state_t& x0,
-                                  const cspace::state_t& xg) {
+                                   const cspace::state_t& xg) {
   double distance = xg[2] - x0[2];
 
   // 1D Movement
@@ -32,8 +32,9 @@ std::vector<double>& move_obstacle(std::vector<double>& obstacle, const cspace::
   return obstacle;
 }
 
-MotionPrimitive::MotionPrimitive() {
+std::vector<trajTuple> MotionPrimitive::generate_1d_primitives() {
   // Create Neutral Motion Primitive
+  std::vector<trajTuple> template_primitives_1D;
   double time_step = param::dt;
   double time_end = 0.2;
 
@@ -43,14 +44,69 @@ MotionPrimitive::MotionPrimitive() {
   std::vector<double> traj_neutral(time.size() * 4, 0.0);
   std::vector<double> input_neutral(time.size(), 0.0);
 
-  template_primitives.push_back(std::make_tuple(time, traj_neutral, input_neutral));
+  template_primitives_1D.push_back(std::make_tuple(time, traj_neutral, input_neutral));
 
   // Create Motion Primitives
   static constexpr double start_position = 0.0, start_speed = 0.0;
-  template_primitives.reserve(speeds.size());
+  template_primitives_1D.reserve(speeds.size());
 
   for (std::size_t i = 1; i < speeds.size(); i++) {
-    template_primitives.push_back(get_zv_trajectory(start_position, start_speed, speeds[i]));
+    template_primitives_1D.push_back(get_zv_trajectory(start_position, start_speed, speeds[i]));
+  }
+  return template_primitives_1D;
+}
+
+void equilize_primitive_length(trajTuple& primitive1, trajTuple& primitive2) {
+  auto extend = [](std::vector<double>& time, std::vector<double>& traj, std::vector<double>& input,
+                   size_t newSize) {
+    if (time.empty()) return;  // Guard against empty vectors
+    double lastTime = time.back(), timeStep = time.size() > 1 ? time[1] - time[0] : 1;
+    for (size_t i = time.size(); i < newSize; ++i) {
+      lastTime += timeStep;
+      time.push_back(lastTime);
+      input.push_back(0);               // Extend input with zeros
+      if (traj.size() / 4 < newSize) {  // Extend traj if needed
+        size_t lastIdx = traj.size() - 4;
+        double x = traj[lastIdx] + traj[lastIdx + 1] * timeStep;
+        double z = traj[lastIdx + 2] + traj[lastIdx + 3] * timeStep;
+        traj.insert(traj.end(), {x, traj[lastIdx + 1], z, traj[lastIdx + 3]});  // Assuming constant velocity
+      }
+    }
+  };
+
+  size_t maxSize = std::max(std::get<0>(primitive1).size(), std::get<0>(primitive2).size());
+  extend(std::get<0>(primitive1), std::get<1>(primitive1), std::get<2>(primitive1), maxSize);
+  extend(std::get<0>(primitive2), std::get<1>(primitive2), std::get<2>(primitive2), maxSize);
+}
+
+MotionPrimitive::MotionPrimitive() {
+  std::vector<trajTuple> template_primitives_1D = generate_1d_primitives();
+  std::size_t index_x = 0;
+  for (auto primitive_x : template_primitives_1D) {
+    std::size_t index_y = 0;
+    for (auto primitive_y : template_primitives_1D) {
+      equilize_primitive_length(primitive_x, primitive_y);
+      auto& [time_x, traj_x, input_x] = primitive_x;
+      auto& [time_y, traj_y, input_y] = primitive_y;
+      std::vector<double> traj;
+      std::vector<double> input;
+      for (std::size_t j = 0; j < time_x.size(); j++) {
+        traj.push_back(traj_x[j * 4]);
+        traj.push_back(traj_x[j * 4 + 1]);
+        traj.push_back(traj_x[j * 4 + 2]);
+        traj.push_back(traj_x[j * 4 + 3]);
+        traj.push_back(traj_y[j * 4]);
+        traj.push_back(traj_y[j * 4 + 1]);
+        traj.push_back(traj_y[j * 4 + 2]);
+        traj.push_back(traj_y[j * 4 + 3]);
+        input.push_back(input_x[j]);
+        input.push_back(input_y[j]);
+      }
+      speeds_xy.push_back({speeds[index_x], speeds[index_y]});
+      template_primitives.push_back(std::make_tuple(time_x, traj, input));
+      index_y++;
+    }
+    index_x++;
   }
 }
 
@@ -59,19 +115,24 @@ std::vector<trajTuple> MotionPrimitive::operator()(const cspace::state_t x) {
   std::vector<trajTuple> primitives;
   primitives.reserve(template_primitives.size());
 
-  for (std::size_t i = 0; i < template_primitives.size(); i++) {
-    std::vector<double> traj;
-    // No step bigger than max speed nor exceeding the max speed
-    if (std::abs(speeds[i] + x[3]) <= max_speed) {
-      for (std::size_t j = 0; j < std::get<1>(template_primitives[i]).size(); j += dim) {
-        double t = std::get<0>(template_primitives[i])[j / 4];
-        traj.push_back(std::get<1>(template_primitives[i])[j]);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 1]);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 2] + x[2] + x[3] * t);
-        traj.push_back(std::get<1>(template_primitives[i])[j + 3] + x[3]);
+  std::size_t speed_index = 0;
+  for (auto& [time_temp, traj_temp, input_temp] : template_primitives) {
+    auto& [speed_x, speed_y] = speeds_xy[speed_index];
+    double speed_squar = std::pow(speed_x + x[3], 2) + std::pow(speed_y + x[7], 2);
+    if (speed_squar <= max_speed * max_speed) {
+      std::vector<double> traj;
+      for (std::size_t j = 0; j < traj_temp.size(); j += dim) {
+        double t = time_temp[j / 8];
+        traj.push_back(traj_temp[j]);
+        traj.push_back(traj_temp[j + 1]);
+        traj.push_back(traj_temp[j + 2] + x[2] + x[3] * t);
+        traj.push_back(traj_temp[j + 3] + x[3]);
+        traj.push_back(traj_temp[j + 4]);
+        traj.push_back(traj_temp[j + 5]);
+        traj.push_back(traj_temp[j + 6] + x[6] + x[7] * t);
+        traj.push_back(traj_temp[j + 7] + x[7]);
       }
-      primitives.push_back(
-          std::make_tuple(std::get<0>(template_primitives[i]), traj, std::get<2>(template_primitives[i])));
+      primitives.push_back(std::make_tuple(time_temp, traj, input_temp));
     }
   }
   return primitives;
@@ -81,26 +142,32 @@ int main() {
   MotionPrimitive primitives;
 
   // Define Start and Goal
-  cspace::state_t x0 = {0, 0, 0, 0};
-  cspace::state_t xg = {0, 0, 1, 0.0};
+  cspace::state_t x0 = {0, 0, 0, 0, 0, 0, 0, 0};
+  cspace::state_t xg = {0, 0, 0.2, 0, 0, 0, 0.2, 0};
 
-  cspace::Options opt(2000, {
+  cspace::Options opt(20000, {
+                                std::make_pair(0.0, 0.0),
+                                std::make_pair(0.0, 0.0),
+                                std::make_pair(-3, 3),
+                                std::make_pair(-0.5, 0.5),
                                 std::make_pair(0.0, 0.0),
                                 std::make_pair(0.0, 0.0),
                                 std::make_pair(-3, 3),
                                 std::make_pair(-0.5, 0.5),
                             });
   // Define Obstacles
-  cspace::DynamicObstacle upper_cart({0.0, 0.0, 0, 0.01, 0, 0, 0.01, 0.01, 0, 0.0, 0.01, 0}, 4, move_obstacle,x0);
-  cspace::StaticObstacle block({0.5, 0.0, 0, 0.51, 0, 0, 0.51, 0.01, 0, 0.5, 0.01, 0}, 4);
-  //opt.dynamic_obstacles.push_back(upper_cart);
-  //opt.static_obstacles.push_back(block);
+  // cspace::DynamicObstacle upper_cart({0.0, 0.0, 0, 0.01, 0, 0, 0.01, 0.01, 0, 0.0, 0.01, 0}, 4,
+  // move_obstacle,
+  //                                   x0);
+  // cspace::StaticObstacle block({0.5, 0.0, 0, 0.51, 0, 0, 0.51, 0.01, 0, 0.5, 0.01, 0}, 4);
+  // opt.dynamic_obstacles.push_back(upper_cart);
+  // opt.static_obstacles.push_back(block);
 
-  cspace::state_t x_test = {0, 0, 0.0, 0.1};
+  cspace::state_t x_test = {0, 0, 0.0, 0.1, 0, 0, 0.1, 0.1};
   auto primitives_x0 = primitives(x_test);
   std::cout << "Number of primitives: " << primitives_x0.size() << std::endl;
-  plot_states_and_input(std::get<0>(primitives_x0[1]), std::get<1>(primitives_x0[1]),
-                       std::get<2>(primitives_x0[1]));
+  //plot_states_and_input(std::get<0>(primitives_x0[1]), std::get<1>(primitives_x0[1]),
+  //                      std::get<2>(primitives_x0[1]));
   plot_primitives(primitives_x0);
 
   // Results
@@ -108,17 +175,21 @@ int main() {
   std::cout << "Success: " << G.get_success() << std::endl;
   std::cout << "Number of Vertices: " << G.size_vertices() << std::endl;
   std::cout << "Initial State: " << G.front().state->at(0) << " " << G.front().state->at(1) << " "
-            << G.front().state->at(2) << " " << G.front().state->at(3) << std::endl;
+            << G.front().state->at(2) << " " << G.front().state->at(3) 
+            << " " << G.front().state->at(4) << " " << G.front().state->at(5)
+            << " " << G.front().state->at(6) << " " << G.front().state->at(7) << std::endl;
   std::cout << "Final State: " << G.back().state->at(0) << " " << G.back().state->at(1) << " "
-            << G.back().state->at(2) << " " << G.back().state->at(3) << std::endl;
+            << G.back().state->at(2) << " " << G.back().state->at(3)
+            << " " << G.back().state->at(4) << " " << G.back().state->at(5)
+            << " " << G.back().state->at(6) << " " << G.back().state->at(7) << std::endl;
   plot_graph(G, x0, xg);
   trajectory_t traj = G.get_trajectory(G.back().state);
-  auto [input,time] = G.get_input(G.back().state);
-  auto traj_from_input = simulate_system({0, 0, 0, 0}, input, param::dt);
+  auto [input, time] = G.get_input(G.back().state);
+  auto traj_from_input = simulate_system(x0, input, param::dt);
   plot_trajectory(traj);
-  plot_trajectory_time(traj, time);
-  plot_trajectory_flat(traj_from_input, time);
-  plot_input(input, time);
+  //plot_trajectory_time(traj, time);
+  //plot_trajectory_flat(traj_from_input, time);
+  //plot_input(input, time);
 
   return 0;
 }
--- examples/2DMotionPrimitives/plot.h
+++ examples/2DMotionPrimitives/plot.h
@@ -7,104 +7,92 @@
 
 using namespace cspace;
 
-void plot_states_and_input(cvec_double& time, cvec_double& states, cvec_double& input, std::size_t dim = 4) {
+void plot_states_and_input(cvec_double& time, cvec_double& states, cvec_double& input, std::size_t dim = 8) {
   namespace plt = matplot;
 
   // Repackage the states
-  std::vector<double> x, xd, z, zd;
+  std::vector<double> x, xd, z, zd, x_x, xd_x, z_x, zd_x, x_y, xd_y, z_y, zd_y;
 
-  for (std::size_t i = 4; i < states.size(); i += dim) {
-    x.push_back(states.at(i));
-    xd.push_back(states.at(i + 1));
-    z.push_back(states.at(i + 2));
-    zd.push_back(states.at(i + 3));
+  for (std::size_t i = 8; i < states.size(); i += dim) {
+    x_x.push_back(states.at(i));
+    xd_x.push_back(states.at(i + 1));
+    z_x.push_back(states.at(i + 2));
+    zd_x.push_back(states.at(i + 3));
+    x_y.push_back(states.at(i + 4));
+    xd_y.push_back(states.at(i + 5));
+    z_y.push_back(states.at(i + 6));
+    zd_y.push_back(states.at(i + 7));
   }
   // Plot the states
   auto h = plt::figure(true);
   h->size(800, 800);
   h->position({0, 0, 600, 600});
-  plt::tiledlayout(3, 1);
 
-  plt::nexttile();
-  plt::plot(time, x)->line_width(2);
-  plt::hold(true);
-  plt::plot(time, xd)->line_width(2);
-  plt::title("x and x_p");
+  plt::plot(time, x_y)->line_width(2);
+  plt::title("Position");
   plt::grid(true);
-  plt::legend({"Position", "Speed"});
-
-  plt::nexttile();
-  plt::plot(time, z)->line_width(2);
-  plt::hold(true);
-  plt::grid(true);
-  plt::plot(time, zd)->line_width(2);
-  plt::title("z and z_p");
-
-  plt::nexttile();
-  plt::plot(time, input)->line_width(2);
-  plt::title("Input");
-  plt::grid(true);
-  plt::xlabel("Time (s)");
   plt::show();
 }
 
 void plot_primitives(std::vector<trajTuple>& primitives) {
   namespace plt = matplot;
   auto h = plt::figure(true);
-  h->size(800, 800);
+  h->size(1600, 800);
   h->position({0, 0, 600, 600});
-  plt::tiledlayout(3, 1);
+  plt::tiledlayout(1, 2);
   auto ax0 = plt::nexttile();
-  plt::title(ax0, "z");
+  plt::title(ax0, "Position Placer");
   plt::hold(ax0, true);
   plt::grid(ax0, true);
   auto ax1 = plt::nexttile();
-  plt::title(ax1, "z_p");
+  plt::title(ax1, "Position Base");
   plt::hold(ax1, true);
   plt::grid(ax1, true);
-  auto ax2 = plt::nexttile();
-  plt::title(ax2, "Input");
-  plt::hold(ax2, true);
-  plt::grid(ax2, true);
-  plt::xlabel(ax2, "Time (s)");
 
   for (const auto& [time, states, input] : primitives) {
-    std::vector<double> x, xd, z, zd;
-    for (std::size_t i = 4; i < states.size(); i += 4) {
-      x.push_back(states[i]);
-      xd.push_back(states[i + 1]);
-      z.push_back(states[i + 2]);
-      zd.push_back(states[i + 3]);
+    std::vector<double> x, xd, z, zd, x_x, xd_x, z_x, zd_x, x_y, xd_y, z_y, zd_y;
+    for (std::size_t i = 8; i < states.size(); i += 8) {
+      x_x.push_back(states.at(i));
+      xd_x.push_back(states.at(i + 1));
+      z_x.push_back(states.at(i + 2));
+      zd_x.push_back(states.at(i + 3));
+      x_y.push_back(states.at(i + 4));
+      xd_y.push_back(states.at(i + 5));
+      z_y.push_back(states.at(i + 6));
+      zd_y.push_back(states.at(i + 7));
     }
 
-    plt::plot(ax0, time, z)->line_width(2);
-    plt::plot(ax1, time, zd)->line_width(2);
-    plt::plot(ax2, time, input)->line_width(2);
+    plt::plot(ax0, z_x, z_y)->line_width(2);
+    plt::plot(ax1, x_x, x_y)->line_width(2);
   }
   plt::show();
 }
 
 void plot_graph(Graph& G, cspace::state_t x0, cspace::state_t xg) {
   namespace plt = matplot;
-  std::vector<double> x, xp, z, zp;
+  std::vector<double> x, xd, z, zd, x_x, xd_x, z_x, zd_x, x_y, xd_y, z_y, zd_y;
 
-  for ([[maybe_unused]] const auto& [state, out, in] : G) {
-    if (state->size() == 4) {
-      x.push_back(state->at(0));
-      xp.push_back(state->at(1));
-      z.push_back(state->at(2));
-      zp.push_back(state->at(3));
+  for ([[maybe_unused]] const auto& [states, out, in] : G) {
+    if (states->size() == 8) {
+      x_x.push_back(states->at(0));
+      xd_x.push_back(states->at(1));
+      z_x.push_back(states->at(2));
+      zd_x.push_back(states->at(3));
+      x_y.push_back(states->at(4));
+      xd_y.push_back(states->at(5));
+      z_y.push_back(states->at(6));
+      zd_y.push_back(states->at(7));
     }
   }
   plt::figure();
-  plt::plot(z, zp, ".")->marker_size(5);
+  plt::plot(z_x, z_y, ".")->marker_size(5);
   plt::hold(true);
-  plt::plot({x0[2]}, {x0[3]}, "bx")->marker_size(15);
-  plt::plot({xg[2]}, {xg[3]}, "gx")->marker_size(15);
+  plt::plot({x0[2]}, {x0[6]}, "bx")->marker_size(15);
+  plt::plot({xg[2]}, {xg[6]}, "gx")->marker_size(15);
   plt::legend({"States", "Start", "Goal"});
   plt::grid(true);
-  plt::xlabel("z");
-  plt::ylabel("z_p");
+  plt::xlabel("z_x");
+  plt::ylabel("z_y");
   plt::show();
 }
 
@@ -112,24 +100,25 @@ void plot_trajectory(const std::vector<std::vector<double>>& traj) {
   namespace plt = matplot;
   // Calculate Time
 
-  std::vector<double> x;
-  std::vector<double> xp;
-  std::vector<double> z;
-  std::vector<double> zp;
+  std::vector<double> x, xd, z, zd, x_x, xd_x, z_x, zd_x, x_y, xd_y, z_y, zd_y;
 
   for (auto& xi : traj) {
-    x.push_back(xi[0]);
-    xp.push_back(xi[1]);
-    z.push_back(xi[2]);
-    zp.push_back(xi[3]);
+    x_x.push_back(xi[0]);
+    xd_x.push_back(xi[1]);
+    z_x.push_back(xi[2]);
+    zd_x.push_back(xi[3]);
+    x_y.push_back(xi[4]);
+    xd_y.push_back(xi[5]);
+    z_y.push_back(xi[6]);
+    zd_y.push_back(xi[7]);
   }
 
   plt::figure();
-  plt::plot(z, zp)->line_width(2);
+  plt::plot(z_x, z_y)->line_width(2);
   plt::hold(true);
-  plt::plot(z, zp, ".")->marker_size(5);
-  plt::xlabel("z");
-  plt::ylabel("z_p");
+  plt::plot(z_x, z_y, ".")->marker_size(5);
+  plt::xlabel("z_x");
+  plt::ylabel("z_y");
   plt::title("Trajectory");
   plt::show();
 }
